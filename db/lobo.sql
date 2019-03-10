fa40363e916de8dcf003cf7f7acde6ae7d60ab07
Jonatan Cerezuela López <jcerezuelalopez@gmail.com>
2019-03-10 19:04:49 +0100

    Modelo fisico ampliado

--- db/lobo.sql
+++ db/lobo.sql
@@ -6,18 +6,18 @@ DROP TABLE IF EXISTS colores CASCADE;
 
 CREATE TABLE colores
 (
-      id BIGSERIAL PRIMARY KEY
+      id     BIGSERIAL    PRIMARY KEY
     , nombre VARCHAR(255) UNIQUE
-    , color VARCHAR(255) NOT NULL
-                           UNIQUE
-                           CHECK (color ~* '^#[a-fA-F0-9]{6}')
+    , color  VARCHAR(255) NOT NULL
+                          UNIQUE
+                          CHECK (color ~* '^#[a-fA-F0-9]{6}')
 );
 
 DROP TABLE IF EXISTS especies CASCADE;
 
 CREATE TABLE especies
 (
-      id BIGSERIAL PRIMARY KEY
+      id      BIGSERIAL    PRIMARY KEY
     , especie VARCHAR(255) NOT NULL
                            UNIQUE
 );
@@ -26,13 +26,13 @@ DROP TABLE IF EXISTS razas CASCADE;
 
 CREATE TABLE razas
 (
-      id BIGSERIAL PRIMARY KEY
-    , raza VARCHAR(255) NOT NULL
-                        UNIQUE
-    , especie_id BIGINT NOT NULL
-                        REFERENCES especies(id)
-                        ON UPDATE CASCADE
-                        ON DELETE NO ACTION
+      id         BIGSERIAL    PRIMARY KEY
+    , raza       VARCHAR(255) NOT NULL
+                              UNIQUE
+    , especie_id BIGINT       NOT NULL
+                              REFERENCES especies(id)
+                              ON UPDATE CASCADE
+                              ON DELETE NO ACTION
     , UNIQUE(raza,especie_id)
 );
 
@@ -40,14 +40,32 @@ DROP TABLE IF EXISTS animales CASCADE;
 
 CREATE TABLE animales
 (
-      id BIGSERIAL PRIMARY KEY
-    , nombre VARCHAR(255) NOT NULL
-    , nacimiento TIMESTAMP
-    , chip VARCHAR(255) UNIQUE
-    , peso NUMERIC(5,2)
-    , ppp  BOOL DEFAULT FALSE
-    , sexo VARCHAR(6)   CONSTRAINT ck_sexo_valido
-                        CHECK (sexo = 'h' OR sexo = 'm') --HEMBRA/MACHO--
+      id            BIGSERIAL    PRIMARY KEY
+    , nombre        VARCHAR(255) NOT NULL
+    , nacimiento                 TIMESTAMP
+    , chip          VARCHAR(255) UNIQUE
+    , peso          NUMERIC(5,2)
+    , ppp           BOOL         DEFAULT FALSE
+    , sexo          VARCHAR(6)   CONSTRAINT ck_sexo_valido
+                                 CHECK (sexo = 'h' OR sexo = 'm') --HEMBRA/MACHO--
     , observaciones TEXT
-    , created_at TIMESTAMP DEFAULT LOCALTIMESTAMP
+    , created_at    TIMESTAMP    DEFAULT LOCALTIMESTAMP
+    , updated_at    TIMESTAMP    DEFAULT LOCALTIMESTAMP
+);
+
+DROP TABLE IF EXISTS animales_razas CASCADE;
+
+CREATE TABLE animales_razas
+(
+      id        BIGSERIAL PRIMARY KEY
+    , animal_id BIGINT    NOT NULL
+                          REFERENCES animales(id)
+                          ON UPDATE CASCADE
+                          ON DELETE NO ACTION
+    , raza_id   BIGINT    NOT NULL
+                          REFERENCES razas(id)
+                          ON UPDATE CASCADE
+                          ON DELETE NO ACTION
+    , UNIQUE(animal_id, raza_id)
+
 );
