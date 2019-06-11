49cc7f808cb6ccd6c41faeb3dd82365b46648b90
Jonatan Cerezuela López <proyectos@xerintel.es>
2019-06-11 14:56:40 +0200

    main mod

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -31,8 +31,9 @@ AppAsset::register($this);
  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
  <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
  <link rel="stylesheet" href="css/jquery.justified.css" />
+ <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
  <script src="js/jquery.justified.min.js"></script>
- <script src="js/notify.min.js"></script>
+ <script src="js/jquery-ui.min.js"></script>
 
 
 </head>
@@ -42,7 +43,7 @@ AppAsset::register($this);
 <div class="wrap" itemscope itemtype="http://schema.org/Organization">
     <?php
     NavBar::begin([
-        'brandLabel' => '<img src="logo.png" itemprop="logo" alt="logo de lobo" class="pull-left brand-logo"/>',
+        'brandLabel' => '<img src="logo.png"  itemprop="logo" alt="logo de lobo" class="pull-left brand-logo"/>',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -52,7 +53,6 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home','url' => ['site/index']],
             ['label' => 'Animales','url' => ['animales/index']],
             '<li><a href="'. Url::to(['site/alertas']) .'">Alertas <span class="badge">'. $alertas .'</span></a></li>',
             ['label' => 'Tratamientos','url' => ['tratamientos/index']],
@@ -63,6 +63,9 @@ AppAsset::register($this);
                     ['label' => 'Colores','url' => ['colores/index']],
                     ['label' => 'Grupos','url' => ['especies/index']],
                     ['label' => 'Enfermedades', 'url' => ['/enfermedades/index']],
+                    !Yii::$app->user->isGuest && Yii::$app->user->identity->rol_id == 1 ? (
+                        ['label' => 'Usuarios', 'url' => ['/usuarios-info/index']]
+                        ) : ( ['label' => '', 'url' => ''] ),
                 ],
             ],
             Yii::$app->user->isGuest ? (
@@ -77,7 +80,7 @@ AppAsset::register($this);
                 )
                 . Html::endForm()
                 . '</li>'
-            )
+            ),
         ],
     ]);
     NavBar::end();
@@ -95,11 +98,13 @@ AppAsset::register($this);
 <footer class="footer">
     <div class="container">
         <p class="pull-left"> Lobo <?= date('Y') ?></p>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+        <script src="js/notify.min.js"></script>
+
 
     </div>
 
 </footer>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 <?php $this->endBody() ?>
 </body>
